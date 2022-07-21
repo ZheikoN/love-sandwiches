c73448ddf1c4489a2db5737a81463533f5295421
ZheikoN <negru.inger@gmail.com>
2022-07-21 11:04:37 +0000

    adjust vcalidation to check that all values are integers

--- run.py
+++ run.py
@@ -32,7 +32,9 @@ def validate_data(values):
     raises valueerror if strings cannot be conferted,
     or if there arent exactly 6 values
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
